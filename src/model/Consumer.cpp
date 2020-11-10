19e6257565cf517c4566fe58aa743627e31e73cf
Robin Middelanis <robin.middelanis@pik-potsdam.de>
2020-11-10 18:03:03 +0100

    revert fix

--- src/model/Consumer.cpp
+++ src/model/Consumer.cpp
@@ -54,7 +54,7 @@ void Consumer::iterate_consumption_and_production() {
         }
         assert(reservation_price > 0.0);
 
-        const Flow desired_used_flow_U_tilde = Flow(round(is->baseline_used_flow_U_star().get_quantity() * forcing_
+        const Flow desired_used_flow_U_tilde = Flow(round(is->initial_used_flow_U_star().get_quantity() * forcing_
                                                           * pow(reservation_price / Price(1.0), is->parameters().consumption_price_elasticity)),
                                                     reservation_price);
         const Flow used_flow_U = Flow(std::min(desired_used_flow_U_tilde.get_quantity(), possible_used_flow_U_hat.get_quantity()), reservation_price);
