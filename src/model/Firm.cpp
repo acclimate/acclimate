7ad674fa61203d5bf154831d035de282ebd35c26
Maria Zioga <mazioga@yahoo.com>
2022-10-24 17:46:13 +0200

    fixing declaration

--- src/model/Firm.cpp
+++ src/model/Firm.cpp
@@ -38,7 +38,7 @@ Firm::Firm(id_t id_p, Sector* sector_p, Region* region_p, const Ratio& possible_
       sector(sector_p),
       capacity_manager(new CapacityManager(this, possible_overcapacity_ratio_beta_p)),
       sales_manager(new SalesManager(this))
-     {financial_sector->( sector_p->name()== "FINC" );
+     {financial_sector = (sector_p->name()=="FINC");
     }
 
 void Firm::produce_X() {
